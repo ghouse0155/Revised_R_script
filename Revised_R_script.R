f5eda941938d4be09af95a26c6494a5b1d95bc9f
ghouse0155 <123266282+ghouse0155@users.noreply.github.com>
2023-04-16 20:43:48 -0400

    Add files via upload

---
+++ Revised_R_script.R
@@ -0,0 +1,75 @@
+#Week 4: dplyr package
+install.packages('dplyr')
+library(dplyr) # Loading Dplyr package
+
+#Task: Write the function to get a dataset from Base R: Titanic
+data_titanic <- read.csv('C:\Users\mghou\Downloads\Titanic.csv')
+
+#and give the dataframe a new name of your choice
+#(hint: you will want your data to be a dataframe. Use the function: as.data.frame(Titanic))
+data_df1 <- data.frame(data_titanic)
+
+#See the top rows of the data
+#TASK: Write the function to see the top rows of the data
+head(data_df1)
+
+#Install and call the package dplyr
+#TASK: Write the functions to install and call dplyr
+
+
+#Let's just see the Survived and Sex columns
+#Task: Write the function to 'select' the Survived and Sex columns 
+#(hint: use the 'select' function)
+selected_df <- select(data_df1,Survived,Sex)
+
+#Let's name the dataset with just the two columns, Survived and Sex
+#TASK: Write the function to save the two columns as one new dataset
+#and give it a name
+saveRDS(selected_df, file = "df_data.RDS") 
+df2<- readRDS("df_data.RDS")
+head(df_new)
+
+#Let's get rid of the Sex column in the new dataset created above
+#TASK: Write the function that deselects the sex column
+#(hint: use the 'select' function to not select a -column)
+df_remove <- df2 %>% select(-one_of("Sex"))
+head(df_remove)
+
+#Let's rename a column name
+#TASK: Write the function that renames 'Sex' to 'Gender'
+
+#Let's make a new dataframe with the new column name
+#TASK: Write the function that names a new dataset that includes the 'gender' column
+gender_df <- data.frame(data_df1 %>% rename("Gender" = "Sex"))
+head(gender_df)
+
+#Let's 'filter' just the males from our dataset
+#TASK: Write the function that includes only rows that are 'male'
+male_df <- filter(gender_df , Gender == 'Male')
+head(male_df)
+
+
+#Let's 'arrange' our data by gender (not the data you just filtered)
+#TASK: Write the function to group the data by gender (hint: arrange())
+arrange(gender_df, Gender) 
+
+#Let's see how many people were examined in the dataset (total the frequency in the original dataframe)
+#TASK: Sum the Freq column
+#TASK: After you run it, write the total here:_2201___
+sum(data_df1$Freq)
+
+
+#Since we have a males dataset, let's make a females dataset
+#TASK: Write the function that includes only rows that are 'female'
+female_df <- filter(gender_df , Gender == 'Female')
+head(female_df)
+
+#And now let's join the males and females
+#TASK: Write the function that joins the male and female rows 
+#(hint: try using 'union' or 'bind_rows')
+male_female_df <- bind_rows(male_df,female_df)
+male_female_df
+
+#Optional Task: add any of the other functions 
+#you learned about from the dplyr package
+
